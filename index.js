2d989a28afdf2a8fc99003ef6c19e6cd2a9ed711
Darren Gantt <DarrenGanttMacbook@192.168.1.6>
2020-09-19 22:04:26 -0400

    updated js file to catch error JSON message and updated html form placeholder content

--- index.js
+++ index.js
@@ -2,14 +2,19 @@
 
 function handleDogImg(responseJson) {
     let randomDogImg = responseJson.message;
-    $(".results").append(`<div class="dog-img"><img src="${randomDogImg}" alt="Picture of dog"></div>`);
+    let responseStatus = responseJson.status;
+    if (responseStatus === "error"){
+        alert(randomDogImg);
+    }
+    else {
+        $(".results").append(`<div class="dog-img"><img src="${randomDogImg}" alt="Picture of dog"></div>`);
+    }
 }
 
 function getDogImages(userInput) {
     fetch(`https://dog.ceo/api/breed/${userInput}/images/random`)
         .then(response => response.json())
         .then(responseJson => handleDogImg(responseJson))
-        .catch(error => alert("Dog Breed could not be found."));
 }
 
 function handleSubmitClicked() {
@@ -23,7 +28,6 @@ function handleSubmitClicked() {
             $(".results").empty();            
             $(".results").append("<h2>Here is a random picture of a fur baby you asked for!</h2>")
             getDogImages(userInput);
-            console.log(userInput);
         }
     });
 }
